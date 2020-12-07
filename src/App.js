af569ecc9de89c6b862b36370833a71416d8527c
satishgaikwad4849 <satishgaikwad4849@gmail.com>
2020-12-07 14:09:29 +0530

    scroll button remove

--- src/App.js
+++ src/App.js
@@ -33,50 +33,50 @@ import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
-function ScrollTop(props) {
-  const { children, window } = props;
-  const classes = useStyles();
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-    disableHysteresis: true,
-    threshold: 100,
-  });
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     position: 'fixed',
+//     bottom: theme.spacing(2),
+//     right: theme.spacing(2),
+//   },
+// }));
+// function ScrollTop(props) {
+//   const { children, window } = props;
+//   const classes = useStyles();
+//   // Note that you normally won't need to set the window ref as useScrollTrigger
+//   // will default to window.
+//   // This is only being set here because the demo is in an iframe.
+//   const trigger = useScrollTrigger({
+//     target: window ? window() : undefined,
+//     disableHysteresis: true,
+//     threshold: 100,
+//   });
 
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+//   const handleClick = (event) => {
+//     const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
 
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  };
+//     if (anchor) {
+//       anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+//     }
+//   };
 
-  return (
-    <Zoom in={trigger}>
-      <div onClick={handleClick} role="presentation" className={classes.root}>
-        {children}
-      </div>
-    </Zoom>
-  );
-}
+  // return (
+//     <Zoom in={trigger}>
+//       <div onClick={handleClick} role="presentation" className={classes.root}>
+//         {children}
+//       </div>
+//     </Zoom>
+//   );
+// }
 
-ScrollTop.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
+// ScrollTop.propTypes = {
+//   children: PropTypes.element.isRequired,
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window: PropTypes.func,
+// };
 const sagaMiddleware = createSagaMiddleware();
  const store = createStore(Rootreducer,applyMiddleware(sagaMiddleware, logger));
  sagaMiddleware.run(rootSaga);
@@ -162,8 +162,8 @@ const sagaMiddleware = createSagaMiddleware();
         </NavItem>
     </SideNav.Nav>
 </SideNav>
-  <CssBaseline />
-      <AppBar>
+   <CssBaseline />
+     {/* <AppBar>
         <Toolbar>
           <Typography variant="h6">Scroll to see button</Typography>
         </Toolbar>
@@ -185,7 +185,7 @@ Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
         <Fab color="secondary" size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
         </Fab>
-      </ScrollTop>
+      </ScrollTop> */}
     </div>
   </div>
      
